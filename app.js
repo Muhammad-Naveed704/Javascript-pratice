4c1889603cc5fc89e37f91c3a4c9027fc0219c90
Muhammad Naveed <epicplayz.147@gmail.com>
2023-04-11 13:06:04 +0500

    if else
    js if else

---
+++ app.js
@@ -0,0 +1,6 @@
+let userAge = +prompt("enter your age");
+if (userAge<=10 ) {
+    alert("Ap Ye Game khel sakte hn")    
+}else{
+    alert("ap ke age zeyadah h ap ye game nhi kel  sakte ye sirf 11 sal se choty bacho k leye h ")
+}
